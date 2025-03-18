51d07fe690851fb6446840164d845bd29509ed8e
Keerill220 <kponomarenko161@gmail.com>
2025-03-18 14:38:50 +0200

    Added input templates for new student information

--- students.js
+++ students.js
@@ -2,12 +2,28 @@ document.addEventListener('DOMContentLoaded', function() {
     const addButton = document.getElementById('btnAdd');
     const popupWindowContainer = document.querySelector('.popupAddContainer');
     const popupWindow = document.querySelector('.popupAdd');
+    const closeButton = document.getElementById('btnClose');
+    const saveButton = document.getElementById('btnSave');
     
     addButton.addEventListener('click', function() {
         popupWindowContainer.style.opacity = '100%';
         popupWindowContainer.style.pointerEvents = 'all';
         popupWindow.style.marginTop = '0';
         popupWindow.style.backgroundColor = 'rgb(255,255,255)';
+    });
+
+    closeButton.addEventListener('click', function() {
+        popupWindowContainer.style.opacity = '0';
+        popupWindowContainer.style.pointerEvents = 'none';
+        popupWindow.style.marginTop = '-100%';
+        popupWindow.style.backgroundColor ='rgb(137, 137, 137)';
+    });
+
+    saveButton.addEventListener('click', function() {
+        popupWindowContainer.style.opacity = '0';
+        popupWindowContainer.style.pointerEvents = 'none';
+        popupWindow.style.marginTop = '-100%';
+        popupWindow.style.backgroundColor ='rgb(137, 137, 137)';
     });
 
 });
