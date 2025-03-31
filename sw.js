25d817591b6fee11e470ed59130ac0448b5e4f28
Keerill220 <kponomarenko161@gmail.com>
2025-03-31 12:10:28 +0300

    Added sw.js and manifest.json for offline access and PWA

---
+++ sw.js
@@ -0,0 +1,55 @@
+const CACHE_NAME = "pwa-cache-v1";
+
+const ASSETS = [
+    "/",
+    "/index.html",
+    "/navigation.css",
+    "/script.js",
+    "/students.js",
+    "/students.css",
+    "/students.html",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Кешування ресурсів...");
+
+      return cache.addAll(ASSETS).catch(console.error);
+    })
+  );
+});
+
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.match(event.request).then((cachedResponse) => {
+
+        const networkFetch = fetch(event.request).then((networkResponse) => {
+
+            cache.put(event.request, networkResponse.clone());
+            return networkResponse;
+        });
+
+        return cachedResponse || networkFetch;
+      });
+    })
+  );
+});
+
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      );
+    }).then(() => {
+      console.log("Новий Service Worker активовано.");
+      return self.clients.claim();
+    })
+  );
+});
